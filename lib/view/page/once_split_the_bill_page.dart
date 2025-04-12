46c472b9f499ce453112a3c59249c272b8ea10cc
tsujinoryuushi <tjnryj0807@icloud.com>
2025-04-12 18:22:01 +0900

    feat：go_routerを追加

---
+++ lib/view/page/once_split_the_bill_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class OnceSplitTheBillPage extends HookConsumerWidget {
+  const OnceSplitTheBillPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const SizedBox();
+  }
+}

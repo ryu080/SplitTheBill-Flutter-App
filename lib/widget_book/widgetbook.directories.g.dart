e1c7a33c77916bc6bee4ad922a31873dfef9d39d
tsujinoryuushi <tjnryj0807@icloud.com>
2025-04-13 20:23:57 +0900

    feat：追加

---
+++ lib/widget_book/widgetbook.directories.g.dart
@@ -0,0 +1,34 @@
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: unused_import, prefer_relative_imports, directives_ordering
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AppGenerator
+// **************************************************************************
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:split_the_bill_app/widget_book/components/styled_text.dart'
+    as _i2;
+import 'package:widgetbook/widgetbook.dart' as _i1;
+
+final directories = <_i1.WidgetbookNode>[
+  _i1.WidgetbookCategory(
+    name: 'widgets',
+    children: [
+      _i1.WidgetbookFolder(
+        name: 'StyledText',
+        children: [
+          _i1.WidgetbookLeafComponent(
+            name: 'StyledText',
+            useCase: _i1.WidgetbookUseCase(
+              name: 'StyledText',
+              builder: _i2.styledTex,
+            ),
+          )
+        ],
+      )
+    ],
+  )
+];
